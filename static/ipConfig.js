c7938753241528bbee05522dde1ac6dbfb25e6e6
fei4980 <921180501@qq.com>
2021-11-25 11:38:10 +0800

    修改查询条件

--- static/ipConfig.js
+++ static/ipConfig.js
@@ -2,7 +2,7 @@
 // const IPCONFIG = 'http://192.168.10.151:8088/kb'
 // const IPCONFIG = 'http://47.103.69.104:8088/kb' 测试
 // eslint-disable-next-line no-unused-vars
-const IPCONFIG = 'http://112.74.85.149:19015/kb'; // 有大量测试业务看板，有用
+const IPCONFIG = 'http://112.74.85.149:19015/kb'; // 有大量测试业务看板，有用！
 // const IPCONFIG = 'http://192.168.1.39:8088/kb'
 // const IPCONFIG = 'http://localhost:8088/kb'
 // const IPCONFIG = 'https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin';
